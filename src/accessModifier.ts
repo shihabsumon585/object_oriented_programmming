63bb83493ffcbcffdceb0e160134dbe5d0a3b453
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-06-13 03:17:00 +0600

    access modifier

---
+++ src/accessModifier.ts
@@ -0,0 +1,14 @@
+
+// access >> modify
+
+class BankAccount {
+    public readonly userId: number;
+    userName: string;
+    protected userBalance: number;
+
+    constructor(userId: number, userName: string, userBalance: number) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userBalance = userBalance;
+    }
+}
