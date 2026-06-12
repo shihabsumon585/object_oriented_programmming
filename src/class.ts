bd79c6fc92b70e79272683c99fc612b94a6551b8
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-06-12 23:14:12 +0600

    class, constructor, method

---
+++ src/class.ts
@@ -0,0 +1,32 @@
+// oop >> class --> object
+
+// class Animal {
+//     name: string
+//     species: string
+//     sound: string
+
+//     constructor(name: string, species: string, sound: string) {
+//         this.name = name;
+//         this.species = species;
+//         this.sound = sound;
+//     }
+
+//     makeSound() {
+//         console.log(`${this.name} is making sound: ${this.sound}`)
+//     }
+// }
+
+
+class Animal {
+    constructor(public name: string, public species: string, public sound: string) {}
+
+    makeSound() {
+        console.log(`${this.name} is making sound: ${this.sound}`)
+    }
+}
+
+const dog = new Animal("dogesh bhai", "dog", "Ghew ghew");
+
+const cat = new Animal("Biral bhai", "Cat", "Mew mew");
+
+dog.makeSound();
