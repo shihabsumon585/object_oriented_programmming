c351c68a0849abcc044da767cdaa796c9ec12f6a
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-06-14 01:21:37 +0600

    encapsulation, the 4th pillar of OOP

---
+++ src/encapsulation.ts
@@ -0,0 +1,19 @@
+
+
+// encapsulation, the 4th pillar of OOP
+
+
+// access >> modify
+
+class BankAccount {
+    public readonly userId: number;
+    userName: string;
+    protected userBalance: number;
+
+    constructor(userId: number, userName: string, userBalance: number) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userBalance = userBalance;
+    }
+}
+
